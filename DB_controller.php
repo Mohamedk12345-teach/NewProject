6b77c4928c98ac2d1c90c203be1eca35516c8a8e
Mohamedk12345-teach <61918067+Mohamedk12345-teach@users.noreply.github.com>
2020-05-08 02:59:00 +0300

    First Commit

---
+++ DB_controller.php
@@ -0,0 +1,25 @@
+<?php
+
+//$servername = "localhost";
+//$username = "id2352126_root";
+//$pass = "";
+//$dbname = "project";
+//	
+	
+	define("DB_Server" , 'localhost');
+	define("DB_Username" , 'root');
+	define("DB_Password" , '');
+	define("DB_Name" , 'projects');
+	
+	$link =  mysqli_connect(DB_Server,DB_Username,DB_Password,DB_Name);
+
+	if(!$link){
+		
+		die("connection failed");
+	}
+	else{
+			
+		
+	}
+
+?>
